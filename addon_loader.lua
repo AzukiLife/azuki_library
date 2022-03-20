e378f3c76827cd184c1cea687f24b428f72ce399
mkxto <zqrk.pers@gmail.com>
2022-03-20 15:25:00 +0100

    Fixed type + renamed base file

--- addon_loader.lua
+++ addon_loader.lua
@@ -1,8 +1,8 @@
 local addon_name = "addon_name" -- eg ULX
 local addon_version = "addon_version" -- eg 1.0
 
-hook.Add("AuzkiLibrary:Ready", "__"..addon_name..addon_version.."AZLIB-Load", function()
-    if AuzkiLibrary then 
+hook.Add("AzukiLibrary:Ready", "__"..addon_name..addon_version.."AzukiLibrary-Load", function()
+    if AzukiLibrary then 
         --[[
             If your addon need to load certain files first, add them like this in preload table.
             You can load anything from client to server, file will load in the order you give them.
@@ -12,8 +12,8 @@ hook.Add("AuzkiLibrary:Ready", "__"..addon_name..addon_version.."AZLIB-Load", fu
             -- {target, file}
             {"sh", "config.lua"} -- First parameter only accept "sh", "sv" and "cl"
         }
-        AuzkiLibrary.Initialize(addon_name, addon_version, preload)
+        AzukiLibrary.Initialize(addon_name, addon_version, preload)
     else
-        print("[!] Fatal Error: Problemes occured while loading Auzki Library")    
+        print("[!] Fatal Error: Problemes occured while using Auzki Library")    
     end
 end)
--- lua/autorun/alib_loader.lua
+++ lua/autorun/alib_loader.lua
@@ -1,4 +1,4 @@
-AuzkiLibrary = AuzkiLibrary or {}
+AzukiLibrary = AzukiLibrary or {}
 
 -- ~ brv
 
@@ -11,11 +11,11 @@ local preloadedFiles = {}
 hook.Add("AzukiLibrary:Loading", "__AzukiLibrary:Loading", function() -- Loading this file
     MsgC(Color(255, 255, 255), "(-) ", Color(161, 99, 212),"Azuki Library",Color(255, 255, 255)," is preparing\n")
 end)
-hook.Add("AuzkiLibrary:Loaded", "__AzukiLibrary:Loaded", function() -- This file finished loading
+hook.Add("AzukiLibrary:Loaded", "__AzukiLibrary:Loaded", function() -- This file finished loading
     MsgC(Color(255, 255, 255), "(~) ",Color(161, 99, 212),"Azuki Library",Color(255, 255, 255)," is loaded\n")
 end)
-hook.Add("AuzkiLibrary:Ready", "__AzukiLibrary:Ready") -- GM:PreGamemodeLoaded is called, this hook is called too
-hook.Add("AuzkiLibrary:AddonsLoaded", "__AzukiLibrary:AddonsLoaded", function (addon_name, addon)
+hook.Add("AzukiLibrary:Ready", "__AzukiLibrary:Ready") -- GM:PreGamemodeLoaded is called, this hook is called too
+hook.Add("AzukiLibrary:AddonsLoaded", "__AzukiLibrary:AddonsLoaded", function (addon_name, addon)
 
 end) -- All the addons finished loading.
 
@@ -30,22 +30,22 @@ local completeColor = Color(84, 185, 143)
 
 -- Print beautiful messages
 local function PrintC(message, prefix)
-    if prefix then prefix = "("..prefix..") " else prefix = "" end
+    if prefix then prefix = "["..prefix.."] " else prefix = "" end
     if string.match(prefix, "SERVER") then
-        MsgC(baseColor, "[AzLib] ", serverColor, prefix, baseColor, message.."\n")
+        MsgC(baseColor, "[aLib] ", serverColor, prefix, baseColor, message.."\n")
     elseif string.match(prefix, "CLIENT") then
-        MsgC(baseColor, "[AzLib] ", clientColor, prefix, baseColor, message.."\n")
+        MsgC(baseColor, "[aLib] ", clientColor, prefix, baseColor, message.."\n")
     elseif string.match(prefix, "SHARED") then
-        MsgC(baseColor, "[AzLib] ", sharedColor, prefix, baseColor, message.."\n")
+        MsgC(baseColor, "[aLib] ", sharedColor, prefix, baseColor, message.."\n")
     elseif string.match(prefix, "COMPLETE") then
-        MsgC(baseColor, "[AzLib] ", completeColor, prefix, baseColor, message.."\n")
+        MsgC(baseColor, "[aLib] ", completeColor, prefix, baseColor, message.."\n")
     else
-        MsgC(baseColor, "[AzLib] "..prefix..message.."\n")
+        MsgC(baseColor, "[aLib] "..prefix..message.."\n")
     end
 end
 
 -- Load a file from a given directory and file
-function AuzkiLibrary.LoadFile(directory, file, log)
+function AzukiLibrary.LoadFile(directory, file, log)
     loadedFiles = loadedFiles + 1
     local author
     if log then  
@@ -62,21 +62,21 @@ function AuzkiLibrary.LoadFile(directory, file, log)
 end
 
 -- Load all files from a given directory
-function AuzkiLibrary.LoadDirectory(directory)
+function AzukiLibrary.LoadDirectory(directory)
     local files, dirs = file.Find(directory.."*", "LUA")
     -- Loop trough all files in the current dir
     for _, file in ipairs(files) do
         if string.match(file, ".lua") and !preloadedFiles[file] then
-            AuzkiLibrary.LoadFile(directory, file, true)
+            AzukiLibrary.LoadFile(directory, file, true)
         end
     end
     for _, dir in ipairs(dirs) do
-        AuzkiLibrary.LoadDirectory(directory..dir.."/")
+        AzukiLibrary.LoadDirectory(directory..dir.."/")
     end
 end
 
 -- Preload Function
-function AuzkiLibrary.PreloadFile(addon_name, preload)
+function AzukiLibrary.PreloadFile(addon_name, preload)
     if not preload then return end -- Preload = nil
     for _, content in pairs(preload) do
         local target = content[1]
@@ -87,11 +87,11 @@ function AuzkiLibrary.PreloadFile(addon_name, preload)
         end
         target = addon_name.."/"..target.."/"
         preloadedFiles[file] = true
-        AuzkiLibrary.LoadFile(target, file, true)
+        AzukiLibrary.LoadFile(target, file, true)
     end
 end
 
-function AuzkiLibrary.Initialize(addon_name, addon_version, preload)
+function AzukiLibrary.Initialize(addon_name, addon_version, preload)
     -- Init vars
     loadedFiles = 0
     preloadedFiles = {}
@@ -107,29 +107,29 @@ function AuzkiLibrary.Initialize(addon_name, addon_version, preload)
     local banner = "=============== "..addon_name.." - "..addon_version.." ==============="
     PrintC(banner)
     -- Preload
-    AuzkiLibrary.PreloadFile(addon_name, preload)
+    AzukiLibrary.PreloadFile(addon_name, preload)
     -- Server
     if SERVER then
-        AuzkiLibrary.LoadDirectory(server_dir)
+        AzukiLibrary.LoadDirectory(server_dir)
     end
     -- Shared
-    AuzkiLibrary.LoadDirectory(shared_dir)
+    AzukiLibrary.LoadDirectory(shared_dir)
     -- Client
-    AuzkiLibrary.LoadDirectory(client_dir)
+    AzukiLibrary.LoadDirectory(client_dir)
     PrintC("Loaded "..loadedFiles.." lua files", "COMPLETE")
     PrintC(string.rep("=", #banner).."\n")
 end
 
-function AuzkiLibrary.ThrowError(addon_name, message)
+function AzukiLibrary.ThrowError(addon_name, message)
     print("")
     print("["..addon_name.."] Thrown an error : ")
     print("[!] "..message)
     print("")
 end
 
-hook.Run("AuzkiLibrary:Loaded")
+hook.Run("AzukiLibrary:Loaded")
 
 hook.Add("PreGamemodeLoaded", "__AzukiLibrary-Waiting", function()
     MsgC(Color(255, 255, 255), "(!) ",Color(161, 99, 212),"Azuki Library",Color(255, 255, 255)," should be ready to load addons\n")
-    hook.Run("AuzkiLibrary:Ready")
+    hook.Run("AzukiLibrary:Ready")
 end)
